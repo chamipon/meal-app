036092054ba273cf60acf5fb165b81d506ecf80b
chamipon <zkucera14@gmail.com>
2024-11-27 18:05:04 -0500

    Added Ingredients and Foods database tables

--- dataset/init.sql
+++ dataset/init.sql
@@ -1,9 +1,44 @@
--- CREATE TYPE status AS ENUM ('pending','in progress','completed');
-
-CREATE TABLE meals (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  description VARCHAR(255),
-  created_date DATE NOT NULL DEFAULT CURRENT_DATE,
-  updated_at DATE NOT NULL DEFAULT CURRENT_DATE
+CREATE TABLE "meals" (
+  "id" SERIAL PRIMARY KEY,
+  "title" varchar,
+  "food_ids" int[],
+  "created_at" timestamp
 );
+
+CREATE TABLE "ingredients" (
+  "id" integer PRIMARY KEY,
+  "title" varchar,
+  "created_at" timestamp
+);
+
+CREATE TABLE "foods" (
+  "id" integer PRIMARY KEY,
+  "title" varchar,
+  "ingredient_ids" int[]
+);
+
+COMMENT ON COLUMN "meals"."food_ids" IS 'Food items in the meal';
+
+COMMENT ON COLUMN "foods"."ingredient_ids" IS 'Ingredients in the food';
+
+CREATE TABLE "meals_foods" (
+  "meals_food_ids" int[],
+  "foods_id" integer,
+  PRIMARY KEY ("meals_food_ids", "foods_id")
+);
+
+ALTER TABLE "meals_foods" ADD FOREIGN KEY ("meals_food_ids") REFERENCES "meals" ("food_ids");
+
+ALTER TABLE "meals_foods" ADD FOREIGN KEY ("foods_id") REFERENCES "foods" ("id");
+
+
+CREATE TABLE "ingredients_foods" (
+  "ingredients_id" integer,
+  "foods_ingredient_ids" int[],
+  PRIMARY KEY ("ingredients_id", "foods_ingredient_ids")
+);
+
+ALTER TABLE "ingredients_foods" ADD FOREIGN KEY ("ingredients_id") REFERENCES "ingredients" ("id");
+
+ALTER TABLE "ingredients_foods" ADD FOREIGN KEY ("foods_ingredient_ids") REFERENCES "foods" ("ingredient_ids");
+
